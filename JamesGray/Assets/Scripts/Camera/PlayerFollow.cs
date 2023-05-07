a62e2c994050a29eecdfa2d30744e2182d627216
임권 <limkwon18@gmail.com>
2023-05-07 16:26:26 +0900

    Dialogue Update
    다이얼로그 위치 변경 및 카메라 효과 추가

--- JamesGray/Assets/Scripts/Camera/PlayerFollow.cs
+++ JamesGray/Assets/Scripts/Camera/PlayerFollow.cs
@@ -6,15 +6,25 @@ using UnityEngine;
 public class PlayerFollow : MonoBehaviour
 {
     public float camera_Speed;
-
+    public Vector2 dialogue_mod;
+    bool dialogueOn = false;
     public GameObject player;
 
     private void Update() 
-    {
+    {   
+        sbyte mod = 0;      // -128 ~ 127
         Vector3 dir = player.transform.position - this.transform.position;
-
-        Vector3 moveVector = new Vector3(dir.x * camera_Speed * Time.deltaTime, dir.y * camera_Speed * Time.deltaTime, 0.0f);
+        if(dialogueOn)
+        {
+            mod = 1;
+        }
+        Vector3 moveVector = new Vector3((dir.x * camera_Speed + (dialogue_mod.x * mod)) * Time.deltaTime, (dir.y * camera_Speed + (dialogue_mod.y * mod)) * Time.deltaTime, 0.0f);
 
         this.transform.Translate(moveVector);
+    }
+
+    public void SetFlag()   //이벤트로 호출되면 플레그를 반전, 다이얼로그에서 호출
+    {
+        dialogueOn = !dialogueOn;
     }
 }
