5dba5d2cb9015c40a295f396a2b5893c37ae04bd
임권 <limkwon18@gmail.com>
2023-05-12 23:38:53 +0900

    플레이어 고정 카메라 스크립트 추가
    기존 카메라 워크를 제거하고 개발 문서에서 요구하는 새로운 카메라 워크 도입.

---
+++ JamesGray/Assets/Scripts/Camera/FixedFollowCamera.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+    플레이어를 화면 중앙에 고정시키는 카메라 움직임
+*/
+public class FixedFollowCamera : MonoBehaviour
+{
+    [Header("다이얼로그 위치 조정")]
+    [Range(-15, 15)]
+    public float x;
+    [Range(-15, 15)]
+    public float y;
+
+    [Header("다이얼로그 위치 이동 속도")]
+    [Range(1, 25f)]
+    public float dialSpeed;
+
+    bool dialogueOn = false;
+    public GameObject player;
+    Vector3 dialVec, playerPos;
+    Coroutine co;
+
+    private void Awake() 
+    {
+        playerPos = player.transform.position;
+        dialVec = new Vector3(x, y, -10f);
+        co = null;    
+    }
+
+    private void Update() 
+    {
+        playerPos = player.transform.position;
+        playerPos.z = -10;
+
+        if(co == null)
+        {
+            transform.position = playerPos;
+        }
+    }
+
+    IEnumerator MoveDialogue()
+    {
+        Vector3 dest = transform.position + dialVec;
+        while(true)
+        {
+            if(dialogueOn)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, dest, Time.deltaTime * dialSpeed);
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, playerPos, Time.deltaTime * dialSpeed);
+            }
+            if(transform.position == player.transform.position) break;
+
+            yield return null;
+        }
+        co = null;
+    }
+
+    public void SetFlag()   //이벤트로 호출되면 플레그를 반전, 다이얼로그에서 호출
+    {
+        dialogueOn = !dialogueOn;
+        if(co != null) StopCoroutine(co);
+
+        co = StartCoroutine(MoveDialogue());
+    }
+}
