0818924fda95292b0c801cc78054a5751b32f49d
임권 <limkwon18@gmail.com>
2023-05-31 00:32:08 +0900

    A* 알고리즘 개선
    이전 위치를 기억하여 경로를 산출할 수 있도록 하였음

--- JamesGray/Assets/Scripts/Algorithm/PathFinding.cs
+++ JamesGray/Assets/Scripts/Algorithm/PathFinding.cs
@@ -10,12 +10,14 @@ namespace PathFinding
     public class AStar
     {
         public LinkedList<Vector2Int> Close = new LinkedList<Vector2Int>();
+        public LinkedList<Vector2Int> ClosePre = new LinkedList<Vector2Int>();
         Vector2Int[] way = {new Vector2Int(1, 0), new Vector2Int(-1, 0), new Vector2Int(0, 1), new Vector2Int(0, -1)};
-        
+        public Vector3Int start, end;
         public AStar(ref Tilemap map, ref Tilemap colider, Vector2Int start, Vector2Int end)
         {
             CalcPath(ref map, ref colider, start, end);
-            //CalcPath(ref PositionManager.tilemap, ref PositionManager.border, start, end);
+            this.start = (Vector3Int)start;
+            this.end = (Vector3Int)end;
         }
 
         void CalcPath(ref Tilemap map, ref Tilemap colider, Vector2Int start, Vector2Int end)
@@ -24,7 +26,7 @@ namespace PathFinding
             LinkedList<Vector2Int> OpenList = new LinkedList<Vector2Int>();
 
             ANode startNode = new ANode();
-            startNode.pos = start;
+            startNode.pos = start; startNode.prePos = start;
             startNode.g = 0; startNode.h = CalcHeuristic(start, end);
             Open.Enqueue(startNode, startNode.f);
             OpenList.AddFirst(startNode.pos);
@@ -38,6 +40,7 @@ namespace PathFinding
                     continue;
 
                 Close.AddFirst(temp.pos);
+                ClosePre.AddFirst(temp.prePos);
 
                 if(temp.pos == end) break;  //목적지 도착시 종료
 
@@ -50,12 +53,18 @@ namespace PathFinding
                     if(OpenList.Contains(temp.pos + way[i]))
                         continue;
                     ANode iter = new ANode();
-                    iter.pos = temp.pos + way[i];
+                    iter.pos = temp.pos + way[i]; iter.prePos = temp.pos;
                     iter.g = temp.g + 1; iter.h = CalcHeuristic(iter.pos, end);
                     Open.Enqueue(iter, iter.f);
                     OpenList.AddFirst(iter.pos);
                 }
             }
+
+            while(Open.count > 0)//비우기
+            {
+                Open.Dequeue();
+            }
+            OpenList.Clear();
         }
         bool CheckTile(ref Tilemap colider, Vector2Int tilePos)
         {
@@ -69,18 +78,17 @@ namespace PathFinding
             return true;
         }
 
-        int CalcHeuristic(Vector2Int start, Vector2Int end)
-        {
+        int CalcHeuristic(Vector2Int start, Vector2Int end) //피타고라스를 이용하여 대각선 값을 구해야 빠르게 동작, 맨하탄을 기준으로 구하면 매우 비효율적
+        {  
             int a = Mathf.Abs(start.x - end.x);
             int b = Mathf.Abs(start.y - end.y);
-            //return Mathf.Abs(start.x - end.x) + Mathf.Abs(start.y - end.y);
             return (a * a) + (b * b);
         }
 
         class ANode
         {
             public int g, h;       //g : 출발 노드에서 현재 노드까지 도달하기 위한 최단 비용 , h : 현재 노드에서 목표 노드까지 예상 이동 비용 (휴리스틱 거리 측정값)
-            public Vector2Int pos;
+            public Vector2Int pos, prePos;
             public int f { get { return g + h; } } //휴리스틱 비용 함수 f = g + h
         }
     }
