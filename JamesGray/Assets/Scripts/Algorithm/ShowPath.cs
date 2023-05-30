ff8ac3b003c4ce0f11e170a8ced9af3f72ffcd74
임권 <limkwon18@gmail.com>
2023-05-31 00:31:07 +0900

    디버그 코드 추가
    길찾기 알고리즘을 위한 디버그 코드 추가
    (방향 표시, 순서 표시)

--- JamesGray/Assets/Scripts/Algorithm/ShowPath.cs
+++ JamesGray/Assets/Scripts/Algorithm/ShowPath.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using TMPro;
 
 public class ShowPath : MonoBehaviour
 {
@@ -11,18 +12,51 @@ public class ShowPath : MonoBehaviour
     public Tilemap map;
     public Tilemap border;
 
+    public GameObject Up, Down, Right, Left;
+
     private void Start() 
     {   
         Vector3Int startPos = start.GetComponent<PositionManager>().GetCellPos();
         Vector3Int endPos = end.GetComponent<PositionManager>().GetCellPos();
 
         PathFinding.AStar path = new PathFinding.AStar(ref PositionManager.tilemap, ref PositionManager.border, new Vector2Int(startPos.x, startPos.y), new Vector2Int(endPos.x, endPos.y));
-
+        int count = 0;
         while(path.Close.Count > 0)
         {
             Vector3 pos = map.GetCellCenterWorld((Vector3Int)path.Close.First.Value);
-            Instantiate(waypoint,pos, Quaternion.identity);
+            GameObject temp = Instantiate(waypoint,pos, Quaternion.identity);
+            temp.GetComponent<TMP_Text>().text = count.ToString();
+
+            SetArrow(path.Close.First.Value, path.ClosePre.First.Value);
+
+            path.ClosePre.RemoveFirst();
             path.Close.RemoveFirst();
+            count++;
+        }
+    }
+
+    void SetArrow(Vector2Int current, Vector2Int pre)
+    {
+        Vector3 pos = map.GetCellCenterWorld((Vector3Int)current);
+        Vector2Int temp = current - pre;
+        if(temp == Vector2Int.down)
+        {
+            Instantiate(Up, pos, Quaternion.Euler(0,0,0));
+        }
+        else if(temp == Vector2Int.up)
+        {
+            //Instantiate(Down, pos, Quaternion.identity);
+            Instantiate(Up, pos, Quaternion.Euler(0,0,180));
+        }
+        else if(temp == Vector2Int.left)
+        {
+            //Instantiate(Left, pos, Quaternion.identity);
+            Instantiate(Up, pos, Quaternion.Euler(0,0,-90));
+        }
+        else if(temp == Vector2Int.right)
+        {
+            //Instantiate(Right, pos, Quaternion.identity);
+            Instantiate(Up, pos, Quaternion.Euler(0,0,90));
         }
     }
 }
