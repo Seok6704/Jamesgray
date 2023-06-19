e351312dd0af1e83a014a46da92e31bc4a6b6951
OTON <limkwon18@gmail.com>
2023-06-20 01:11:29 +0900

    Update PositionManager.cs
    로딩화면에서 오류가 생기는 코드 수정
    널참조 문제 방지 코드 추가

--- JamesGray/Assets/Scripts/Move/PositionManager.cs
+++ JamesGray/Assets/Scripts/Move/PositionManager.cs
@@ -14,10 +14,10 @@ public class PositionManager : MonoBehaviour
     public static Tilemap border = null;
     private void Awake() 
     {
-        if(tilemap == null) tilemap = GameObject.FindWithTag("Map").GetComponent<Tilemap>();    //tag가 map으로 지정된 오브젝트에서 타일맵 컴포넌트 불러오기
-        if(border == null) border = GameObject.FindWithTag("Border").GetComponent<Tilemap>();
+        if(tilemap == null) tilemap = GameObject.FindWithTag("Map")?.GetComponent<Tilemap>();    //tag가 map으로 지정된 오브젝트에서 타일맵 컴포넌트 불러오기
+        if(border == null) border = GameObject.FindWithTag("Border")?.GetComponent<Tilemap>();
 
-        Vector3 temp = tilemap.GetCellCenterWorld(tilemap.WorldToCell(this.transform.position));    //위치 변경
+        Vector3 temp = tilemap ? tilemap.GetCellCenterWorld(tilemap.WorldToCell(this.transform.position)) : new Vector3(0f,0f,0f); //위치 변경
 
         this.transform.position = temp;
     }
