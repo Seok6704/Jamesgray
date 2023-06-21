da96cb54c823356cd1d7ab15358f6ef507e91e87
임권 <limkwon18@gmail.com>
2023-06-22 00:47:53 +0900

    인벤토리 스크립트 추가
    인벤토리에 필요한 기본적인 기능 구현

---
+++ JamesGray/Assets/Scripts/Inventory.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+public class Inventory
+{
+    byte index;
+    List<Page> pages;
+
+    public Inventory()
+    {
+        pages = new List<Page>();
+        index = 0;
+        /*
+            추후에 json파일로 인벤토리 내역을 불러오는 로직 추가 예정
+        */
+        for(int i = 0; i < 5; i++)
+            pages.Add(new Page((byte)i));
+    }
+    public int GetPage()   //페이지 정보 불러오기
+    {
+        return pages[index].GetNum();
+    }
+
+    public void MoveLeftPage()   //왼쪽 페이지로 이동
+    {
+        if(index == 0)
+            return;
+
+        index -= 1;
+    }
+
+    public void MoveRightPage()  //오른쪽 페이지로 이동
+    {
+        if(index == pages.Count - 1 || pages.Count == 0)
+            return;
+
+        index += 1;
+    }
+
+    public bool IsLeftEnd()
+    {
+        if(index == 0) return true;
+        return false;
+    }
+    public bool IsRightEnd()
+    {
+        if(index == pages.Count - 1 || pages.Count == 0) return true;
+        return false;
+    }
+
+    class Page
+    {
+        byte num;
+        public Page(byte num)
+        {
+            this.num = num;
+        }
+
+        public int GetNum()
+        {
+            return num;
+        }
+    }
+}
---
+++ JamesGray/Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public UnityEngine.Events.UnityEvent LeftEnd, RightEnd, EnableAll;
+    static Inventory inventory = null;
+    public TMPro.TMP_Text text;
+
+    private void Awake() 
+    {
+        if(inventory == null)   //싱글톤
+            inventory = new Inventory();
+
+        ShowInventory();
+    }
+
+    void ShowInventory()
+    {
+        text.text = (inventory.GetPage() + 1).ToString();
+
+        EnableAll.Invoke(); //일단 모든 화살표를 이벤트로 활성화 한 후, 끝에 도달한 곳은 비활성화.
+
+        if(inventory.IsLeftEnd()) LeftEnd.Invoke();
+        if(inventory.IsRightEnd()) RightEnd.Invoke();    
+    }
+
+    public void GetLeftPage()
+    {
+        inventory.MoveLeftPage();
+        ShowInventory();
+    }
+    public void GetRightPage()
+    {
+        inventory.MoveRightPage();
+        ShowInventory();
+    }
+}
