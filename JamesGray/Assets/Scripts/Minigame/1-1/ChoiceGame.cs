3e618e84dab78234bb21eef1b0820f06e51b5211
Seok6704 <dnflrkcjswo@naver.com>
2023-06-24 06:14:41 +0900

    1-1 미니게임 스크립트 수정
    1-1 미니게임 동작 스크립트 수정하였습니다. 3회 이상 틀릴 경우 실패 처리되도록 변경하였고, 버튼 구동 방식 알고리즘에 조금 변경이 있었습니다

--- JamesGray/Assets/Scripts/Minigame/1-1/ChoiceGame.cs
+++ JamesGray/Assets/Scripts/Minigame/1-1/ChoiceGame.cs
@@ -6,130 +6,109 @@ using UnityEngine.SceneManagement;
 public class ChoiceGame : MonoBehaviour
 {
     public int Correct;
+    public int Fail; // 실패 횟수 기록 변수
     public GameObject Dialog;
 
     void Start()
     {
         Correct = GameObject.Find("Start_Button").GetComponent<NPCManager>().i_Story; //Correct 변수에 Start_Button 오브젝트의 NPCManager 스크립트 안에 i_Stroy 변수 값 저장
+        Fail = 0;
     }
 
     public void Choice_Car()
     {
-        switch (Correct)
+        if(Correct == 4)
         {
-            case 0:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 1:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 2:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 3:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 4:
             Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 6);
             Invoke("SceneChanger", 5f);
-            break;
+        }
+        else
+        {
+            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
+            Fail = Fail + 1;
+            if(Fail > 2)
+            {
+                Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 7);
+                Invoke("SceneChanger", 5f);
+            }
         }
     }
+    
 
     public void Choice_Subway()
     {
-        switch (Correct)
+        if(Correct == 3)
         {
-            case 0:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 1:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 2:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 3:
             Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 6);
             Invoke("SceneChanger", 5f);
-            break;
-            case 4:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
         }
-
+        else
+        {
+            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
+            Fail = Fail + 1;
+            if(Fail > 2)
+            {
+                Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 7);
+                Invoke("SceneChanger", 5f);
+            }
+        }
     }
 
     public void Choice_Ambulance()
     {
-        switch (Correct)
+        if(Correct == 2)
         {
-            case 0:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 1:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 2:
             Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 6);
             Invoke("SceneChanger", 5f);
-            break;
-            case 3:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 4:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
         }
-
+        else
+        {
+            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
+            Fail = Fail + 1;
+            if(Fail > 2)
+            {
+                Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 7);
+                Invoke("SceneChanger", 5f);
+            }
+        }
     }
 
     public void Choice_Phone()
     {
-        switch (Correct)
+        if(Correct == 1)
         {
-            case 0:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 1:
             Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 6);
             Invoke("SceneChanger", 5f);
-            break;
-            case 2:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 3:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 4:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
         }
-
+        else
+        {
+            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
+            Fail = Fail + 1;
+            if(Fail > 2)
+            {
+                Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 7);
+                Invoke("SceneChanger", 5f);
+            }
+        }
     }
 
     public void Choice_Door()
     {
-        switch (Correct)
+        if(Correct == 0)
         {
-            case 0:
             Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 6);
             Invoke("SceneChanger", 5f);
-            break;
-            case 1:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 2:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 3:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
-            case 4:
-            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
-            break;
         }
-
+        else
+        {
+            Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 5);
+            Fail = Fail + 1;
+            if(Fail > 2)
+            {
+                Dialog.GetComponent<DialoguesManager>().SetDialogue(901, 7);
+                Invoke("SceneChanger", 5f);
+            }
+        }
     }
 
     void SceneChanger() //씬 전환 함수
