1b5467fce51394f14f403ef434d1347cc825a605
임권 <limkwon18@gmail.com>
2023-06-24 23:45:40 +0900

    Update SceneController.cs
    Additive 방식으로 씬을 불러오는 경우 오류 방지를 위해 설정이 필요한 코드 추가

--- JamesGray/Assets/Scripts/Loading/SceneController.cs
+++ JamesGray/Assets/Scripts/Loading/SceneController.cs
@@ -2,19 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+    SceneController 스크립트는 LoadingScene 스크립트를 불러와서 로딩 씬으로 전환 후 다음 씬으로 넘어가는 동작을 할 수 있도록 해주는 스크립트입니다.
+    로딩 씬으로 전환하는 이유는 메모리 GC의 동작 순서 때문입니다. 자세한 내용은 검색 혹은 질문해주세요.
+    
+    Function : 
+                public void LoadNextScene(string nextSceneName, float waitTime = 1f, bool isAdditive = false) //로딩 씬을 호추하기 전에 일정시간 대기하도록 하는 함수.
+                waitTime은 초 단위(아마도) 기본값은 1초로 설정되어 있음.
+                isAdditive 는 씬을 Additive로 불러올지 여부 기본적으로는 Async로 불러와짐
+
+                예시, 만약 어떤 씬에서 챕터 0을 불러오고 싶다면 어떤 씬의 종료 부분에서 다음코드를 작성한다. (어떠한 객체가 SceneController를 컴포넌트로 가지고 있어야할 것이다. 아마도)
+                    (SceneController를 가진 어떤 객체).GetComponent<SceneController>().LoadNextScene("Chapter0");
+*/
 public class SceneController : MonoBehaviour
-{
-    public void WaitToLoad()
+{   
+    //이벤트 시스템과 리스너는 한 씬에 두개 있으면 오류가 발생하므로 비활성화 해야하므로 additive로 씬을 호출할경우 비활성화해야함
+    public GameObject audioListner;
+    public GameObject eventSys;
+
+    public void LoadNextScene(string nextSceneName, float waitTime = 1f, bool isAdditive = false)
     {
-        StartCoroutine(Wait());
+        if(!isAdditive)
+            StartCoroutine(Wait(nextSceneName, waitTime));
+        else
+            StartCoroutine(AdditivelyLoad(nextSceneName, waitTime));
+    }
+    public void WaitToLoad()        //챕터 0만 불러오는 함수이므로 다른 함수를 사용하도록 하자
+    {
+        StartCoroutine(Wait("Chapter0", 1f));
     }
 
-    IEnumerator Wait() 
+    public void AdditiveEnded()
     {
-        for(int i = 0; i < 1000; i++)
+        SetEnable(true);
+    }
+
+    IEnumerator Wait(string nextSceneName, float waitTime)
+    {
+        /*for(int i = 0; i < 1000; i++)      
         {
             yield return null;
-        }    
-        LoadingScene.LoadScene("Chapter0");
+        }*/
+        yield return new WaitForSeconds(waitTime); //바로 씬 전환 대신 일정 시간 대기한 후 로딩씬으로 넘어가도록 하는 반복문
+        LoadingScene.LoadScene(nextSceneName);
+    }
+
+    IEnumerator AdditivelyLoad(string nextSceneName, float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        SetEnable(false);
+        LoadingScene.LoadAdditive(nextSceneName);
+    }
+
+    void SetEnable(bool enable) //additive로 씬을 호출했을때, 오류를 발생시킬 여지가 있는 컴포넌트 비활성화 및 재 활성화
+    {
+        audioListner.GetComponent<AudioListener>().enabled = enable;
+        eventSys.GetComponent<UnityEngine.EventSystems.EventSystem>().enabled = enable;
+        eventSys.GetComponent<UnityEngine.EventSystems.StandaloneInputModule>().enabled = enable;
     }
 }
