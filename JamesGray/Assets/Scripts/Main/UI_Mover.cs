f85be6271e5f8ae552e30db68142a84846922f64
임권 <limkwon18@gmail.com>
2023-07-13 21:31:38 +0900

    UI 이동시 관련 이벤트 발생 추가

--- JamesGray/Assets/Scripts/Main/UI_Mover.cs
+++ JamesGray/Assets/Scripts/Main/UI_Mover.cs
@@ -10,7 +10,9 @@ public class UI_Mover : MonoBehaviour
     public bool parentOnStart;  //시작시 캔버스 화면에 위치 (참일때)
     [Header("원하는 위치")]
     public GameObject showPosition;//캔버스 가운데 대신 특정 오브젝트 가운데로 가고 싶을때
-    
+    [Header("활성화 및 비활성화 이벤트")]
+    public UnityEngine.Events.UnityEvent set2Parent, set2Return;    //활성화 비활성화를 알수 있게 이벤트 발생
+
     Vector3 pos;
 
     void Start()
@@ -23,11 +25,13 @@ public class UI_Mover : MonoBehaviour
     public void SetPos2Parent()
     {
         transform.position = transform.parent.position;
+        set2Parent.Invoke();
     }
 
     public void Set2ReturnPos()
     {
         transform.position = pos;
+        set2Return.Invoke();
     }
 
     public void Set2ShowPosition()
