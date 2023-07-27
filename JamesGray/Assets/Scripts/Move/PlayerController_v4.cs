a994ecc04f36ebc07f5cd83738bcc414f3deb377
임권 <limkwon18@gmail.com>
2023-07-27 13:26:09 +0900

    플레이어 무브먼트 version 4 구현 및 코드 최적화
    기존의 version3 에서 코루틴으로 동작하는 것을 제거하여 더욱 간결하게 코드를 재구성 하였습니다.
    또한 타일맵을 통해 좌표를 연산하는 부분도 제거하였습니다.
    마지막으로 기존에 null 검사방식 보다 개선된 null 검사 메소드를 사용하여 최적화를 진행하였습니다.

--- JamesGray/Assets/Scripts/Move/PlayerController_v4.cs
+++ JamesGray/Assets/Scripts/Move/PlayerController_v4.cs
@@ -12,7 +12,7 @@ public class PlayerController_v4 : MonoBehaviour
     [Range(0.001f, 5f)]
     public float speed;
 
-    //public VirtualKeyPad keyPad;    //가상 키패드
+    public VirtualKeyPad keyPad;    //가상 키패드
 
     Vector3Int currentCell;
     Vector3 dirVec;
@@ -39,56 +39,80 @@ public class PlayerController_v4 : MonoBehaviour
 
     void Update() 
     {
-        //Vector3 preDir = new Vector3(animator.GetInteger("Horizontal"), animator.GetInteger("Vertical"), 0);
+        Vector3 preDir = dirVec;    //캐릭터 이동시 방향을 전환해도 애니메이션이 바뀌지 않는 문제가있음(백스텝 밟음) 애니메이션 업데이트를 강제하기위한 변수
 
-        if(Input.GetKey(KeyCode.A) && !isWalk)
+        if(isOnAction) 
         {
-            isWalk = true;
-            targetPos = transform.position;
-            targetPos.x -= 1;
-            dirVec = Vector3.left;
+            animator.SetBool("isWalk", false);
+            return;
         }
-        else if(Input.GetKey(KeyCode.D) && !isWalk)
+
+        RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object"));
+        if(!ReferenceEquals(rayHit.collider, null))
         {
-            isWalk = true;
-            targetPos = transform.position;
-            targetPos.x += 1;
-            dirVec = Vector3.right;
+            tempScanObj = rayHit.collider.gameObject;
         }
-        else if(Input.GetKey(KeyCode.W) && !isWalk)
+        else 
         {
-            isWalk = true;
-            targetPos = transform.position;
-            targetPos.y += 1;
-            dirVec = Vector3.up;
+            tempScanObj = null;
         }
-        else if(Input.GetKey(KeyCode.S) && !isWalk)
+
+        if(!isWalk)
         {
-            isWalk = true;
-            targetPos = transform.position;
-            targetPos.y -= 1;
-            dirVec = Vector3.down;
+            if((Input.GetKeyDown(KeyCode.E) || keyPad.ACTION) && !ReferenceEquals(tempScanObj, null) && tempScanObj.CompareTag("NPC"))
+            {
+                scanObject = tempScanObj;
+                OnAction();
+            }
+            else if((Input.GetKey(KeyCode.A) || keyPad.LEFT) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                dirVec = Vector3.left;
+                if(ReferenceEquals(Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object")).collider, null))
+                { 
+                    isWalk = true;
+                    targetPos = transform.position;
+                    targetPos.x -= 1;
+                }
+            }
+            else if((Input.GetKey(KeyCode.D) || keyPad.RIGHT) || Input.GetKey(KeyCode.RightArrow))
+            {   
+                dirVec = Vector3.right;
+                if(ReferenceEquals(Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object")).collider, null))
+                { 
+                    isWalk = true;
+                    targetPos = transform.position;
+                    targetPos.x += 1; 
+                }
+            }
+            else if((Input.GetKey(KeyCode.W) || keyPad.UP) || Input.GetKey(KeyCode.UpArrow))
+            {
+                dirVec = Vector3.up;
+                if(ReferenceEquals(Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object")).collider, null))
+                { 
+                    isWalk = true;
+                    targetPos = transform.position;
+                    targetPos.y += 1;
+                }
+            }
+            else if((Input.GetKey(KeyCode.S) || keyPad.DOWN) || Input.GetKey(KeyCode.DownArrow))
+            {
+                dirVec = Vector3.down;
+                if(ReferenceEquals(Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object")).collider, null))
+                { 
+                    isWalk = true;
+                    targetPos = transform.position;
+                    targetPos.y -= 1;
+                }
+            }
         }
 
         animator.SetInteger("Horizontal", (int)dirVec.x);
         animator.SetInteger("Vertical", (int)dirVec.y);
-        if(animator.GetBool("isWalk") != isWalk)
+        if(animator.GetBool("isWalk") != isWalk || dirVec != preDir)
         {
             animator.SetBool("isWalk", isWalk);
             if(isWalk) animator.SetTrigger("Walk");
         }
-
-        /*if(dirVec != preDir)
-        {
-            animator.SetInteger("Horizontal", (int)dirVec.x);
-            animator.SetInteger("Vertical", (int)dirVec.y);
-        }
-        if(animator.GetBool("isWalk") != isWalk)
-        {
-            if(isWalk) animator.SetTrigger("Walk");
-            animator.SetBool("isWalk", isWalk);
-        }*/
-        
     }
 
     private void FixedUpdate() 
@@ -104,4 +128,25 @@ public class PlayerController_v4 : MonoBehaviour
         }
     }
 
+    void OnAction()
+    {
+        scanObject.GetComponent<NPCManager>().OnAction();   //모든 Object layer는 NPCManager 스크립트를 가지고 있어야 정상 작동... 
+        onAction.Invoke();
+        isOnAction = true;
+    }
+
+    public void EndAction()
+    {
+        scanObject.GetComponent<NPCManager>().EndAction();
+        //StopCoroutine(co);
+        //co = null;
+        isOnAction = false;
+        scanObject = null;
+        endAction.Invoke();
+    }
+
+    public void ToggleFreeze()    //플레이어 멈추기 입력을 받기를 멈춘다.
+    {
+        isOnFreeze = !isOnFreeze;
+    }
 }
