be00a5b11f6a626feb53af062ad53499e9021f12
임권 <limkwon18@gmail.com>
2023-07-28 13:22:05 +0900

    Update SerialCOM.cs

--- JamesGray/Assets/Scripts/Test/JOYSTICK/SerialCOM.cs
+++ JamesGray/Assets/Scripts/Test/JOYSTICK/SerialCOM.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO.Ports;
 
 //https://velog.io/@anthem53/Unity-Serial-%ED%86%B5%EC%8B%A0-%EB%A9%94%EB%AA%A8 참고
+//https://parksh3641.tistory.com/entry/%EC%9C%A0%EB%8B%88%ED%8B%B0-C-%EB%B8%94%EB%A3%A8%ED%88%AC%EC%8A%A4-%ED%86%B5%EC%8B%A0-%EA%B0%84%EB%8B%A8-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0
 
 /*
     chatgpt 한테 물어본결과
@@ -36,11 +37,12 @@ public class SerialCOM : MonoBehaviour
     {
         if(sp.IsOpen)
         {
-            Debug.Log(sp.ReadExisting());
+            Debug.Log(sp.ReadLine());
         }    
     }
 
     private void OnDestroy() {
-        sp.Close();
+        if(sp.IsOpen)
+            sp.Close();
     }
 }
