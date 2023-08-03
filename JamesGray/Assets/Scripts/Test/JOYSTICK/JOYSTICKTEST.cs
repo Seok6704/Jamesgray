a4dea5f2a8dfb109d803c5027172f142dab357f6
임권 <limkwon18@gmail.com>
2023-08-04 00:03:20 +0900

    조이스틱 입력 UI 표시

--- JamesGray/Assets/Scripts/Test/JOYSTICK/JOYSTICKTEST.cs
+++ JamesGray/Assets/Scripts/Test/JOYSTICK/JOYSTICKTEST.cs
@@ -7,18 +7,48 @@ public class JOYSTICKTEST : MonoBehaviour
 {
     public TMP_Text X, Y;
     public GameObject circle;
+    
+    SerialCOM serial;
 
     Vector3 pos;
     private void Awake() {
         pos = circle.transform.position;
+        serial = GetComponent<SerialCOM>();
         Debug.Log(System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
     }
     private void Update() {
         //Debug.Log("X : " + Input.GetAxis("Horizontal") + "   Y : " + Input.GetAxis("Vertical"));
-        float x = Input.GetAxis("Horizontal"); float y = Input.GetAxis("Vertical");
-        X.text = x.ToString();
-        Y.text = y.ToString();
+        //float x = Input.GetAxis("Horizontal"); float y = Input.GetAxis("Vertical");
+        char input = serial.GetInput();
+        Vector3 dir = Vector3.zero;
 
-        circle.transform.position = new Vector3(pos.x + x, pos.y + y , pos.z);
+        switch(input)
+        {
+            case 'n':
+                dir = Vector3.zero;
+                break;
+            case 'w':
+                dir = Vector3.up;
+                break;
+            case 's':
+                dir = Vector3.down;
+                break;
+            case 'a':
+                dir = Vector3.left;
+                break;
+            case 'd':
+                dir = Vector3.right;
+                break;
+        }
+
+
+        //X.text = x.ToString();
+        //Y.text = y.ToString();
+
+        X.text = dir.x.ToString();
+        Y.text = dir.y.ToString();
+
+
+        circle.transform.position = new Vector3(pos.x + dir.x, pos.y + dir.y , pos.z);
     }
 }
