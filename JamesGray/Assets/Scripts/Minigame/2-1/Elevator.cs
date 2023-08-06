a4e190be76fd63e2a5d0d4256550cc64d5826bb2
Seok6704 <dnflrkcjswo@naver.com>
2023-08-06 23:53:45 +0900

    2-1 Minigame Update

--- JamesGray/Assets/Scripts/Minigame/2-1/Elevator.cs
+++ JamesGray/Assets/Scripts/Minigame/2-1/Elevator.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Elevator : MonoBehaviour
 {
     Animator ele0, ele1, ele2, james;
+    public AudioSource audioSrc;
+    int ran;
 
     void Start()
     {
@@ -12,6 +14,7 @@ public class Elevator : MonoBehaviour
         ele1 = GameObject.Find("Elevator1").GetComponent<Animator>();
         ele2 = GameObject.Find("Elevator2").GetComponent<Animator>();
         james = GameObject.Find("James").GetComponent<Animator>();
+        ran = Random.Range(0,3);
     }
 
     public void LeftElevatorClick()
@@ -36,5 +39,96 @@ public class Elevator : MonoBehaviour
     {
         yield return new WaitForSeconds(1.5f);
         anim.SetTrigger("Open");
+    }
+
+    public void BtnE0Click()
+    {
+        if(ran == 0)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Correct") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else if(ran == 1)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect1") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect2") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+    }
+    public void BtnE1Click()
+    {
+        if(ran == 1)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Correct") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else if(ran == 0)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect2") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect1") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+    }
+    public void BtnE2Click()
+    {
+        if(ran == 2)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Correct") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else if(ran == 0)
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect1") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
+        else
+        {
+            if(audioSrc.isPlaying)
+            {
+                audioSrc.Stop();
+            }
+            AudioClip clip = Resources.Load("Sounds/Minigame/2-1/Incorrect2") as AudioClip;
+            audioSrc.PlayOneShot(clip);
+        }
     }
 }
