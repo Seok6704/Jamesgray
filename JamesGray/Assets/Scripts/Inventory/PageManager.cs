d78f3557f54ae29e2f1302ebb74d88b6ee82ab41
임권 <limkwon18@gmail.com>
2023-08-14 00:35:37 +0900

    Update PageManager.cs

--- JamesGray/Assets/Scripts/Inventory/PageManager.cs
+++ JamesGray/Assets/Scripts/Inventory/PageManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Tools;
 
 /// <summary>
 /// 인벤토리 페이지 마다 구조가 다르므로 이 클래스에서 데이터가 주어지면 알맞은 형식으로 Grid Layout Group을 사용하여 표시하는 역할을 맡는다.
