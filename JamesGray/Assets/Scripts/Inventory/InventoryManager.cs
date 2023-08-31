ca8b0f4f50c437d3c25996c70b616c8da13c9914
임권 <limkwon18@gmail.com>
2023-08-31 15:26:09 +0900

    에셋 번들 사용을 위한 수정

--- JamesGray/Assets/Scripts/Inventory/InventoryManager.cs
+++ JamesGray/Assets/Scripts/Inventory/InventoryManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Tools;
 
 /// <summary>
 /// 인벤토리 표시를 관리하는 클래스, 인벤토리 데이터 생성을 명령하고, 페이지 넘기기 데이터 추가 명령 등 인벤토리 표시 및 관리를 담당
@@ -14,11 +15,19 @@ public class InventoryManager : MonoBehaviour
     public TMPro.TMP_Text text;
 
     Animator animator;
+    
+    static AssetBundleManager assetBundle;
+    static string bundleName = "inventory_assets";
 
     private void Awake() 
     {
         if(inventory == null)   //싱글톤
             inventory = new Inventory();
+        
+        if(assetBundle == null)
+        {
+            assetBundle = new AssetBundleManager(bundleName);
+        }
         
         animator = GetComponent<Animator>();
 
@@ -28,6 +37,7 @@ public class InventoryManager : MonoBehaviour
     void ShowInventory()
     {
         text.text = (inventory.GetPageNum() + 1).ToString();
+        
 
         EnableAll.Invoke(); //일단 모든 화살표를 이벤트로 활성화 한 후, 끝에 도달한 곳은 비활성화.
 
