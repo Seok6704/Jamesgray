7d5a297932e545804d3b900c1009ad31291a296f
임권 <limkwon18@gmail.com>
2023-08-31 15:26:39 +0900

    인벤토리 내용물 관련 업데이트

--- JamesGray/Assets/Scripts/Inventory/PageManager.cs
+++ JamesGray/Assets/Scripts/Inventory/PageManager.cs
@@ -10,14 +10,27 @@ using Tools;
 /// </summary>
 public class PageManager : MonoBehaviour
 {
-    public List<TMP_Text> contents;
+    public List<TMP_Text> texts;
     public List<Image> sprites;
 
-    public void SetContents(List<Inventory.Content> contents)
-    {
-        for(int i = 0; i < sprites.Count; i++)
+    public void SetContents(List<Inventory.Content> contents, AssetBundle assetBundle)
+    {   //굳이 한 for문 안에 다 넣지 않은 이유는 텍스트와 스프라이트의 수가 일치한다는 보장을 할 수 없으므로... 스프라이트만 있는 레이아웃일 경우를 위해서 이와 같이함.
+        int i;
+        for(i = 0; i < sprites.Count; i++)
         {
-            
+            if(assetBundle.Contains(contents[i].sprite))
+            {
+                sprites[i].sprite = assetBundle.LoadAsset<Sprite>(contents[i].sprite);
+            }
+            else
+            {
+                Debug.Log("NO SUCH SPRITE");
+            }
+        }
+
+        for(i = 0; i < texts.Count; i++)
+        {
+            texts[i].text = contents[i].content;
         }
     }
 
