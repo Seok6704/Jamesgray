dda94031a8f494f9f5ef85570674f42e1e8a27ef
Seok6704 <dnflrkcjswo@naver.com>
2023-09-06 23:00:21 +0900

    미니게임 2-3 업데이트
    아직 현재 개발 중 입니다.

---
+++ JamesGray/Assets/Scripts/Minigame/2-3/UnlockGame.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockGame : MonoBehaviour
+{
+    VoiceManager voice = VoiceManager.getInstance();
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
