284852311c9b768053407ee562472eeb37ee909f
임권 <limkwon18@gmail.com>
2023-09-26 01:28:16 +0900

    Update TextOutputManager.cs
    대화 출력 시작, 완료 이벤트 구현 및 개선점 추가

--- JamesGray/Assets/Scripts/Main/TextOutputManager.cs
+++ JamesGray/Assets/Scripts/Main/TextOutputManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using Unity.VisualScripting;
 
 /*
     텍스트를 출력하는 스크립트
@@ -12,7 +13,7 @@ using TMPro;
 
 public class TextOutputManager : MonoBehaviour
 {
-    public UnityEvent typeDone;
+    public UnityEvent typeDone, typeStart;
     [Range(0, 10)]
     [Tooltip("1초")]
     public float f_dial_Speed;  
@@ -22,6 +23,15 @@ public class TextOutputManager : MonoBehaviour
     private void Awake() 
     {
         SetText();
+
+        if(typeDone == null)
+        {
+            typeDone = new UnityEvent();    //이벤트 생성... 스크립트로 이벤트 함수를 사용하려고 하면 이렇게 다들하는데 왜그런지 모르겠음...
+        }
+        if(typeStart == null)
+        {
+            typeStart = new UnityEvent();
+        }
     }
     void SetText() //초기화 함수, 자기자신의 TMPro 컴포넌트를 불러오고, 글자를 모두 초기화한다.
     {
@@ -30,32 +40,38 @@ public class TextOutputManager : MonoBehaviour
     }
 
     public void PrintDirect(string s_dial) //바로 출력하는 함수
-    {    
+    {   
+        StopTyping();
         tmp_Text.text = s_dial;
+        typeDone.Invoke();
     }
 
     public void ClearText() //문장 비우기
     {
+        StopTyping();
         tmp_Text.text = "";
     }
 
     public void StopTyping()    //출력중이라면 멈추기
     {
         if(co != null) StopCoroutine(co);
+        co = null;
     }
 
     public void Typing(string s_dial) //힌글자씩 천천히 출력하는 함수
     {
+        StopTyping();
         co = StartCoroutine(PutText(s_dial));
     }
     IEnumerator PutText(string s_dial) //한글자씩 코루틴으로 출력 Typing 함수가 불러오는 코루틴
     {  //천천히 출력하는 코루틴
+        typeStart.Invoke(); //문장 출력 시작 이벤트
         foreach (char c in s_dial.ToCharArray()) 
         {
             tmp_Text.text += c;
             yield return new WaitForSeconds(f_dial_Speed);
         }
-        typeDone.Invoke();
+        typeDone.Invoke();  //문장 출력 완료 이벤트
     }
 
 }
