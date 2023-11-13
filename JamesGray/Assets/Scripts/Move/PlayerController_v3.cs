de8e15d9d9769b2f45201438eef254fbbedd4830
임권 <limkwon18@gmail.com>
2023-11-14 01:00:18 +0900

    블루투스 컨트롤러 업데이트
    플레이어 컨트롤러에서 블루투스 입력을 받을 수 있도록 하였습니다.

--- JamesGray/Assets/Scripts/Move/PlayerController_v3.cs
+++ JamesGray/Assets/Scripts/Move/PlayerController_v3.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 using UnityEngine.Events;
 
-public class PlayerController_v3 : MonoBehaviour
+public class PlayerController_v3 : MonoBehaviour, IBLE
 {
     public UnityEvent onAction, endAction;
     public Tilemap tileMap;    //타일맵
@@ -25,7 +25,17 @@ public class PlayerController_v3 : MonoBehaviour
     Rigidbody2D rigid;
     //RaycastHit2D rayHit;
 
-    SerialCOM serial; //유선 연결 패드
+    //SerialCOM serial; //유선 연결 패드, 최종은 블루투스이기때문에 사용X
+
+
+    ///BLE변수들
+    private BluetoothController BLE = null;
+
+    public bool BLE_Connection;
+    public bool BLE_Ready2Read;
+    public bool BLE_Scan;
+    string BLEinput;
+    ///BLE end
 
     private void Awake() 
     {
@@ -41,8 +51,8 @@ public class PlayerController_v3 : MonoBehaviour
 
         rigid = GetComponent<Rigidbody2D>();
 
-        serial = new SerialCOM(9600, 8);    //9600hz 11번 포트
-        serial = SerialCOM.getInstance();   //인스턴스 가져오기
+        //serial = new SerialCOM(9600, 8);    //9600hz 11번 포트, 시리얼 사용하지않기
+        //serial = SerialCOM.getInstance();   //인스턴스 가져오기
     }
 
     /*private void FixedUpdate() 
@@ -56,7 +66,8 @@ public class PlayerController_v3 : MonoBehaviour
         Vector3Int nextCell = currentCell;
 
 
-        serial.GetInput();  //SerialCom 객체는 함수로 호출해야 값을 가져온다.
+        //serial.GetInput();  //SerialCom 객체는 함수로 호출해야 값을 가져온다.
+        Read(2);
 
         //Debug.DrawRay(transform.position, dirVec * 0.7f, new Color(0,1,0)); // 게임 뷰에서는 보이지 않지만 플레이 버튼 누르고 씬뷰로 전환하면 보임!
         RaycastHit2D rayHit = Physics2D.Raycast(transform.position, dirVec, 1f, LayerMask.GetMask("Object"));
@@ -80,25 +91,25 @@ public class PlayerController_v3 : MonoBehaviour
                 scanObject = tempScanObj;
                 OnAction();
             }
-            else if(Input.GetKey(KeyCode.W) || keyPad.UP || Input.GetKey(KeyCode.UpArrow) || serial.UP)    
+            else if(Input.GetKey(KeyCode.W) || keyPad.UP || Input.GetKey(KeyCode.UpArrow) || BLEinput == "w")    
             {
                 dirVec = Vector3.up;
                 nextCell.y += 1;
                 co = StartCoroutine(MovePlayer(nextCell));
             }
-            else if(Input.GetKey(KeyCode.S) || keyPad.DOWN || Input.GetKey(KeyCode.DownArrow) || serial.DOWN)
+            else if(Input.GetKey(KeyCode.S) || keyPad.DOWN || Input.GetKey(KeyCode.DownArrow) || BLEinput == "s")
             {
                 dirVec = Vector3.down;
                 nextCell.y -= 1;
                 co = StartCoroutine(MovePlayer(nextCell));
             } 
-            else if(Input.GetKey(KeyCode.A) || keyPad.LEFT || Input.GetKey(KeyCode.LeftArrow) || serial.LEFT)
+            else if(Input.GetKey(KeyCode.A) || keyPad.LEFT || Input.GetKey(KeyCode.LeftArrow) || BLEinput == "a")
             {
                 dirVec = Vector3.left;
                 nextCell.x -= 1;
                 co = StartCoroutine(MovePlayer(nextCell));
             } 
-            else if(Input.GetKey(KeyCode.D) || keyPad.RIGHT || Input.GetKey(KeyCode.RightArrow) || serial.RIGHT)
+            else if(Input.GetKey(KeyCode.D) || keyPad.RIGHT || Input.GetKey(KeyCode.RightArrow) || BLEinput == "d")
             {
                 dirVec = Vector3.right;
                 nextCell.x += 1;
@@ -250,4 +261,90 @@ public class PlayerController_v3 : MonoBehaviour
         isOnFreeze = !isOnFreeze;
         isOnAction = !isOnAction;
     }
+
+    //////////////////////////////////////////BLE////////////////////////////////////////////
+    
+    private void Start() 
+    {
+        //if(SettingManager.useController)
+        //{
+        //   return;
+        //}
+        if (Application.platform != RuntimePlatform.Android) return;
+
+        BLE = BluetoothController.GetInstance();
+        BLE_Connection = false;
+        BLE_Ready2Read = false;
+        BLE_Scan = false;
+        BLEinput = "n";
+        
+        BLE.SetReceiverName(gameObject.name);   //JAVA로 부터 메세지를 받을 객체
+        Debug.Log(gameObject.name + "is Receiver");
+        
+        //ScanBLE();
+    }
+
+    /// <summary>
+    /// 블루투스에게 데이터 요청하기. Characteristic이 여러개인 경우 num으로 지정해야함.
+    /// </summary>
+    /// <param name="num">Characteristic Index</param>
+    public void Read(int num)   //num = 2로 일단 해보기
+    {
+        //Debug.Log("Call READ");
+        if(!BLE_Connection || !BLE_Ready2Read)
+        {
+            BLEinput = "n";
+            return;
+        }   
+        
+        BLE.ReadCharacteristic(num);
+    }
+    
+    /// <summary>
+    /// 컨트롤러 데이터가 들어오는 곳
+    /// </summary>
+    public void OutPutLog(string msg)
+    {
+        Debug.Log("From Java - " + msg);
+        BLEinput = msg;
+        //BLE.SendLog("Unity Receive - " + msg);
+    }
+
+    public void SetBLEConnection(string msg)
+    {
+        if(msg == "TRUE")
+        {
+            BLE_Connection = true;
+        }
+        else
+        {
+            BLE_Connection = false;
+        }
+    }
+
+    public void SetReady2Read(string msg)
+    {
+        if(msg == "TRUE")
+        {
+            BLE_Ready2Read = true;
+        }
+        else
+        {
+            BLE_Ready2Read = false;
+        }
+    }
+    /// <summary>
+    /// 디버그 로그가 들어오는 곳
+    /// </summary>
+    public void setLog(string msg)
+    {
+        Debug.Log("From Java Debug Log - " + msg);
+    }
+
+    public void ScanBLE()
+    {
+        BLE_Scan = !BLE_Scan;
+        BLE.ScanBLE();
+    }
+    
 }
