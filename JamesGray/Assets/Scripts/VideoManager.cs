d9d917e4834d15cd301591e4a4c3f35f9f28b54a
임권 <limkwon18@gmail.com>
2023-11-15 00:44:28 +0900

    비디오 업데이트

--- JamesGray/Assets/Scripts/VideoManager.cs
+++ JamesGray/Assets/Scripts/VideoManager.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.Video;
 
 //https://docs.unity3d.com/kr/2020.3/Manual/Video.html  //참고
 
-public class VideoManager
+public class VideoManager : MonoBehaviour
 {
     //readonly string path = Application.dataPath + "/Videos/NPC/";
     readonly string path = Application.streamingAssetsPath + "/Videos/";
 
     VideoPlayer video;
 
-    public VideoManager(VideoPlayer v, AudioSource audiosrc)
+    public VideoManager(VideoPlayer v, AudioSource audiosrc = null)
     {
         video = v;      //영상을 재생할 플레이어 설정
         //video.SetTargetAudioSource(0, audiosrc);
@@ -26,11 +30,51 @@ public class VideoManager
     public void PlayVideo(string sceneName, int id, int lineID, int index)
     {
         if(video.isPlaying) video.Stop();
+        string url = GetPath(sceneName, id, lineID, index);
+        //string output = Application.persistentDataPath + "/dial123789490123890124590.mp4";
 
-        video.url = GetPath(sceneName, id, lineID, index);
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            //video.url = GetAndroidPath(sceneName, id, lineID, index);
+            //UnityWebRequest w = UnityWebRequest.Get(url);
+            Debug.Log(url);
+            //Debug.Log(output);
 
-        if(!System.IO.File.Exists(video.url)) return;  //파일 존재 여부 확인
+            //StartCoroutine(LoadVideo(url, output));
+            //return;
+        }
+        video.url = url;
 
+        if(!System.IO.File.Exists(video.url)) 
+        {
+            Debug.Log("NO FILE");
+            return;  //파일 존재 여부 확인
+        }
+
+
+
+        Debug.Log("Video is being Ready.");
+        //StartCoroutine(PrepareVideo());
+
+        video.Play();
+    }
+
+    IEnumerator LoadVideo(string url, string output)
+    {
+        UnityWebRequest req = UnityWebRequest.Get(url);
+        req.downloadHandler = new DownloadHandlerFile(output);
+        yield return req.SendWebRequest();
+            
+        if(req.isNetworkError || req.isHttpError) Debug.Log(req.error);
+            
+        yield return new WaitForSeconds(0.1f);
+        
+        video.url = output;
+        if(!System.IO.File.Exists(video.url)) 
+        {
+            Debug.Log("NO FILE");
+            yield break;  //파일 존재 여부 확인
+        }
         video.Play();
     }
 
