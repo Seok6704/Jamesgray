0de4572b235e3a1703c97d0e9a05b591bba3be56
임권 <limkwon18@gmail.com>
2023-11-15 17:31:12 +0900

    Update PlayerController_v3.cs

--- JamesGray/Assets/Scripts/Move/PlayerController_v3.cs
+++ JamesGray/Assets/Scripts/Move/PlayerController_v3.cs
@@ -127,10 +127,6 @@ public class PlayerController_v3 : MonoBehaviour, IBLE
         {
             h = 0; v = 0;
         }
-        else
-        {
-            h = (int)dirVec.x; v = (int)dirVec.y;
-        }
 
         if(animator.GetInteger("hAxisRaw") != h)
         {
@@ -173,14 +169,14 @@ public class PlayerController_v3 : MonoBehaviour, IBLE
             //rigid.MovePosition(towardsPos);
             yield return null;
         }
-        //if(checkKey())
-        //{
-        //    co = StartCoroutine(MovePlayer(new Vector3Int(cellPos.x + (int)dirVec.x, cellPos.y + (int)dirVec.y, cellPos.z)));
-        //}
-        //else
-        //{
+        if(checkKey())
+        {
+            co = StartCoroutine(MovePlayer(new Vector3Int(cellPos.x + (int)dirVec.x, cellPos.y + (int)dirVec.y, cellPos.z)));
+        }
+        else
+        {
             co = null;
-        //}
+        }
         currentCell = cellPos;
     }
     /// <summary>
